5fdd4c867cfb475706e1ca34bdc97458da19cf6d
raghavendra-star <raghuraghaven6@gmail.com>
2020-11-03 23:54:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {Container}  from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+
+//importing components
+import Todo from "./components/todo";
+import TodoForm from "./components/todoform";
+
+//redux
+import {Provider} from "react-redux";
+import store from "./store";
+
+function App() {
+  return (
+    
+    <Provider store={store}>
+      <Container fluid>
+       
+        <Todo/>
+        <TodoForm/>
+      </Container>
+    </Provider>
+  );
+}
+
+export default App;
